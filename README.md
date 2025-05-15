05ee990d8dc56e9678188cde85c15a5700f3da61
rharu <r.haruperi@hotmail.com>
2025-05-15 15:35:36 +0300

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # RH-Thrust Candles
+
+This indicator for cTrader identifies "thrust" candles and colors them based on their strength. A thrust candle is a candle that closes significantly above or below the highs/lows of a specified number of preceding candles.
+
+## How it Works
+
+-   **Bullish Thrust:** A bullish candle is considered a "strong" thrust if its closing price is higher than the high prices of the `Look Back Candles` number of preceding candles. Otherwise, it's considered a "weak" bullish candle.
+-   **Bearish Thrust:** A bearish candle is considered a "strong" thrust if its closing price is lower than the low prices of the `Look Back Candles` number of preceding candles. Otherwise, it's considered a "weak" bearish candle.
+-   **Neutral Candles:** If a candle's open and close prices are the same (e.g., a Doji), its color is not changed by this indicator.
+
+## Parameters
+
+The indicator has the following configurable parameters:
+
+-   **Look Back Candles:**
+    -   Description: The number of previous candles to look back at to determine if the current candle is a strong thrust.
+    -   Default Value: `2`
+    -   Minimum Value: `1`
+
+-   **Strong Bullish Candle Colour:**
+    -   Description: The color for strong bullish thrust candles.
+    -   Default Value: `#26A69A` (a shade of green)
+
+-   **Strong Bearish Candle Colour:**
+    -   Description: The color for strong bearish thrust candles.
+    -   Default Value: `#EF5350` (a shade of red)
+
+-   **Weak Bullish Candle Colour:**
+    -   Description: The color for weak bullish candles (those that are bullish but not strong thrusts).
+    -   Default Value: `#0B2D29` (a dark green/teal)
+
+-   **Weak Bearish Candle Colour:**
+    -   Description: The color for weak bearish candles (those that are bearish but not strong thrusts).
+    -   Default Value: `#420706` (a dark red)
+
+## Outputs
+
+The indicator directly modifies the color of the bars on the chart. It also exposes two `IndicatorDataSeries`:
+
+-   `ThrustCandle`: This series is declared but not explicitly assigned values within the provided `Calculate` method. It might be intended for future use or for other indicators to access thrust candle information.
+-   `ThrustSwitch`: Similar to `ThrustCandle`, this series is declared but not assigned values in the current `Calculate` method.
+
+*Note: The `Initialize` method is currently empty but can be used for any setup logic if needed in the future.*
--- RH-Thrust Candles/RH-Thrust Candles.cs
+++ RH-Thrust Candles/RH-Thrust Candles.cs
@@ -6,27 +6,80 @@ using cAlgo.API.Internals;
 
 namespace cAlgo
 {
-    [Indicator(AccessRights = AccessRights.None)]
+    [Indicator(IsOverlay = true, TimeZone = TimeZones.UTC, AutoRescale =false, AccessRights = AccessRights.None)]
     public class RHThrustCandles : Indicator
     {
-        [Parameter(DefaultValue = "Hello world!")]
-        public string Message { get; set; }
+        [Parameter("Look Back Candles", DefaultValue = 2, MinValue = 1)]
+        public int LookBackCandles { get; set; }
 
-        [Output("Main")]
-        public IndicatorDataSeries Result { get; set; }
+        [Parameter("Strong Bullish Candle Colour", DefaultValue = "#26A69A")]
+        public string StrongBullishColor { get; set; }
+
+        [Parameter("Strong Bearish Candle Colour", DefaultValue = "#EF5350")]
+        public string StrongBearishColor { get; set; }
+
+        [Parameter("Weak Bullish Candle Colour", DefaultValue = "#0B2D29")]
+        public string WeakBullishColor { get; set; }
+
+        [Parameter("Weak Bearish Candle Colour", DefaultValue = "#420706")]
+        public string WeakBearishColor { get; set; }
+
+ 
+        public IndicatorDataSeries ThrustCandle { get; set; }
+        public IndicatorDataSeries ThrustSwitch { get; set; }
 
         protected override void Initialize()
         {
-            // To learn more about cTrader Automate visit our Help Center:
-            // https://help.ctrader.com/ctrader-automate
+            // Initialization logic can be added here if needed in the future.
+        }
 
-            Print(Message);
+        private bool IsStrongBullish(int index)
+        {
+            if (index < LookBackCandles) return false; // Not enough data for lookback
+
+            var currentClose = Bars.ClosePrices[index];
+            for (int i = 1; i <= LookBackCandles; i++)
+            {
+                if (currentClose <= Bars.HighPrices[index - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool IsStrongBearish(int index)
+        {
+            if (index < LookBackCandles) return false; // Not enough data for lookback
+
+            var currentClose = Bars.ClosePrices[index];
+            for (int i = 1; i <= LookBackCandles; i++)
+            {
+                if (currentClose >= Bars.LowPrices[index - i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public override void Calculate(int index)
         {
-            // Calculate value at specified index
-            // Result[index] = 
+            var currentOpen = Bars.OpenPrices[index];
+            var currentClose = Bars.ClosePrices[index];
+    
+
+            if (currentClose > currentOpen) // Bullish candle
+            {
+                bool isStrong = IsStrongBullish(index);
+                Chart.SetBarColor(index, isStrong ? StrongBullishColor : WeakBullishColor);
+            }
+            else if (currentClose < currentOpen) // Bearish candle
+            {
+                bool isStrong = IsStrongBearish(index);
+                Chart.SetBarColor(index, isStrong ? StrongBearishColor : WeakBearishColor);
+            }
+            // If currentClose == currentOpen (Doji or neutral candle), color is not changed.
         }
     }
 }
